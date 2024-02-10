f9a1ba2ac81c43365a2f90b89cb882f9b40b3c51
Jim Pioche <91530212+VictorAgahi@users.noreply.github.com>
2024-02-10 01:57:21 +0000

    tag archi
    e

--- assets/js/car.js
+++ assets/js/car.js
@@ -23,7 +23,7 @@ class Car{
         this.controls=new Controls(controlType);
 
         this.img=new Image();
-        this.img.src="car.png"
+        this.img.src="assets/img/car.png"
 
         this.mask=document.createElement("canvas");
         this.mask.width=width;
